41331cf3bc7d39bacb3fafefcac439adafaa82f4
facu-bon <facu.bonino236@gmail.com>
2026-02-13 09:17:43 -0300

    probando fix en connectionDB

--- src/config/connectionDB.js
+++ src/config/connectionDB.js
@@ -2,13 +2,28 @@ import mongoose from "mongoose";
 import ENVIRONMENT from "./environment.js";
 
 
-async function connectDB(){
-    try {
-        await mongoose.connect(ENVIRONMENT.MONGO_DB_CONNECTION_STRING);
-        console.log("Conectado a la base de datos");
-    } catch (error) {
-        console.log("Error al conectar a la base de datos", error);
-    }
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(ENVIRONMENT.MONGO_DB_CONNECTION_STRING).then((mongoose) => {
+      return mongoose
+    })
+  }
+  try {
+    cached.conn = await cached.promise;
+    console.log("Conectado a la base de datos");
+  } catch (error) {
+    cached.promise = null;
+    console.log("Error al conectar a la base de datos", error);
+  }
+  return cached.conn
 }
 
 export default connectDB
